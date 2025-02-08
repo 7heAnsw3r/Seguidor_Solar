c4becbff84f56a3917978c2fe28ccd2de5309780
ImYasid <yasid.jimenez@outlook.com>
2025-02-07 23:09:11 -0500

    :sparkles: avance interfaz

--- seguidorSolar.py
+++ seguidorSolar.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from PIL import Image, ImageTk
 from tkinter import ttk
 from Interfaz import seleccionarFechas
 
@@ -43,7 +44,7 @@ boton_continuar.pack()
 def abrir_interfaz_principal():
     raiz = tk.Tk()
     raiz.title("Seguidor Solar")
-    raiz.geometry("600x500")
+    raiz.geometry("1200x600")
     raiz.configure(bg="#e6f2ff")  # Fondo azul claro
     raiz.iconbitmap("./Imagenes/panel-solar.ico")
     centrar_ventana(raiz)
@@ -52,20 +53,37 @@ def abrir_interfaz_principal():
     style = ttk.Style()
     style.configure("TFrame", background="#e6f2ff")
     style.configure("TLabelFrame", background="#cce0ff", font=("Arial", 12, "bold"))
-    style.configure("TButton", font=("Arial", 10, "bold"), background="#4da6ff", foreground="white")
+    style.configure("TButton", font=("Arial", 10, "bold"), background="#4da6ff", foreground="black")
 
-    # Frame para datetime_picker
-    datetime_frame = ttk.LabelFrame(raiz, text="Selección de Fecha y Hora", padding=10)
-    datetime_frame.pack(padx=10, pady=10, fill='x')
+     # Panel izquierdo
+    left_panel = tk.Frame(raiz, bg="#ADD8E6", width=300)  # Fondo azul claro con borde
+    left_panel.pack(side=tk.LEFT, fill=tk.Y, pady=10)
 
-    # Botón para abrir el datetime_picker
-    boton_abrir_datetime = ttk.Button(datetime_frame, text="Seleccionar Fecha y Hora", 
-                                    command=seleccionarFechas.crear_ventana_datetime)
+    # Título en el panel izquierdo
+    title_label = tk.Label(left_panel, text="SIMULADOR DE UN \n SEGUIDOR SOLAR", bg="#ADD8E6", font=("Arial", 18, "bold"), fg="black")
+    title_label.pack(pady=10)
+
+    # Logo
+    logo_path = r"C:\Users\User\Desktop\EPN\4. CUARTO SEMESTRE\1. METODOS NUMERICOS\4. PROYECTOS\Seguidor_Solar\Imagenes\panel-solar.png"
+    logo_img = Image.open(logo_path)
+    logo_img = logo_img.resize((120, 120))  # Ajustar tamaño
+    logo_img = ImageTk.PhotoImage(logo_img)
+
+    logo_label = tk.Label(left_panel, image=logo_img, bg="#ADD8E6")
+    logo_label.image = logo_img  # Mantener referencia
+    logo_label.pack(pady=40)
+
+    # Botón de selección de parámetros
+    boton_abrir_datetime = ttk.Button(left_panel, text="Selector de Parámetros", command=seleccionarFechas.crear_ventana_datetime, style="TButton")
     boton_abrir_datetime.pack(pady=10)
 
-    # Frame principal
-    miFrame = ttk.Frame(raiz, padding=20)
-    miFrame.pack(pady=20)
+    # Botón de generación de reporte
+    report_button = ttk.Button(left_panel, text="Generar Reporte", style="TButton") 
+    report_button.pack(pady=10)
+
+    # --- Main Content Area --- 
+    main_area = tk.Frame(raiz, bg="white")  # White background
+    main_area.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 
     raiz.mainloop()
 
