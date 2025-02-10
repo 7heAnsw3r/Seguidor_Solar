a183b1cb999ec126f79187497ebf288d1d49b88d
Iandres10188 <ianandres.onate@epn.edu.ec>
2025-02-10 11:23:40 -0500

    :memo: Add pydocs and comment functions

--- Interfaz/__init__.py
+++ Interfaz/__init__.py
@@ -1,5 +1,11 @@
+"""
+Módulo de inicialización para la carpeta `Interfaz`.
+
+Este archivo permite importar fácilmente las funciones y módulos principales 
+de la interfaz gráfica del simulador de seguidor solar.
+"""
+
+# Importaciones de los módulos internos de la interfaz
 from .seleccionarParametros import obtener_fecha_y_horas, crear_ventana_datetime, st_fecha, start_t, end_t
-
 from .calculoAngulos import Rxyz, getSolarPosition
-
 from .generarReporte import generar_reporte
